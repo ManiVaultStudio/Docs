86a3a2f29f719baf56cb8c5a91d316c64280cff6
Thomas Kroes <tkroes81@gmail.com>
2026-02-05 16:53:08 +0100

    fix: adjust maxdepth in TOC for release notes index

--- .github/scripts/synchronize_releases.py
+++ .github/scripts/synchronize_releases.py
@@ -155,7 +155,7 @@ def render_index_md(
 
     lines.append("")
 
-    lines.append("```{toctree}\n:maxdepth: 2\n")
+    lines.append("```{toctree}\n:maxdepth: 1\n")
 
     # Each link points into the generated directory
     for r in releases:
